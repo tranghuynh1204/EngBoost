29dd6f5dd2eb89331f1fc0e855cd40feaaead950
JAVA_09_21110856 <21110856@student.hcmute.edu.vn>
2024-12-08 15:39:47 +0700

    css

--- toeic-online/app/admin/list-exams/page.tsx
+++ toeic-online/app/admin/list-exams/page.tsx
@@ -22,6 +22,7 @@ import { DateFilter } from "@/components/list-exam/date-filter";
 import { Button } from "@/components/ui/button";
 import { useToast } from "@/hooks/use-toast";
 import { PiExport } from "react-icons/pi";
+import { Input } from "@/components/ui/input";
 export const ExamList = () => {
   const searchParams = useSearchParams();
   const router = useRouter();
@@ -52,7 +53,13 @@ export const ExamList = () => {
       new Set(exams.map((exam) => exam.category).filter(Boolean))
     );
   }, [exams]);
-
+  useEffect(() => {
+    setTitle(searchParams.get("title") || "");
+    setCategory(searchParams.get("category") || "");
+    setStartDate(searchParams.get("startDate") || "");
+    setEndDate(searchParams.get("endDate") || "");
+    setCurrentPage(Number(searchParams.get("page")) || 1);
+  }, [searchParams]);
   // Update query parameters in the URL
   const updateQuery = (newParams: Record<string, string | number>) => {
     const params = new URLSearchParams(searchParams.toString());
@@ -196,28 +203,28 @@ export const ExamList = () => {
     fetchExamData();
   }, [category, title, startDate, endDate, currentPage]);
   return (
-    <div className="p-6 bg-white rounded-lg shadow-md">
-      <h1 className="text-2xl font-semibold text-gray-800 mb-6">
+    <div className="p-6 bg-gray-50 min-h-screen">
+      <h1 className="text-3xl font-semibold text-gray-800 mb-6 text-center">
         List of Exams
       </h1>
 
       {/* File Input for Import and Update */}
-      <div className="mb-6 flex gap-4">
-        <input
+      <div className="mb-6 flex flex-col md:flex-row items-center justify-center gap-4">
+        <Input
           type="file"
           onChange={(e) => setFile(e.target.files ? e.target.files[0] : null)}
-          className="file-input file-input-bordered file-input-primary"
+          className="file-input file-input-bordered file-input-primary w-full md:w-auto"
         />
         <Button
           onClick={handleImport}
-          className="text-white bg-gray-700"
+          className="bg-green-500 hover:bg-green-600 text-white"
           disabled={!file}
         >
           Import Exam
         </Button>
         <Button
           onClick={handleUpdate}
-          className="bg-gray-600 text-white "
+          className="bg-yellow-500 hover:bg-yellow-600 text-white"
           disabled={!file || !updatingId}
         >
           Update Exam
@@ -249,75 +256,112 @@ export const ExamList = () => {
       </div>
 
       {/* Loading and Error States */}
-      {loading && <p className="text-gray-600">Loading exams...</p>}
-      {error && <p className="text-red-600">{error}</p>}
+      {loading && <p className="text-center text-gray-600">Loading exams...</p>}
+      {error && <p className="text-center text-red-600">{error}</p>}
       {!loading && !error && exams.length === 0 && (
-        <p className="text-gray-600">No exams found.</p>
+        <p className="text-center text-gray-600">No exams found.</p>
       )}
 
       {/* Exam Table */}
       {!loading && !error && exams.length > 0 && (
-        <Table className="min-w-full bg-white border border-gray-200 rounded-lg">
-          <TableHeader>
-            <TableRow>
-              <TableHead className="px-4 py-3 text-left">Title</TableHead>
-              <TableHead className="px-4 py-3 text-left">Category</TableHead>
-              <TableHead className="px-4 py-3 text-left">
-                Duration (mins)
-              </TableHead>
-              <TableHead className="px-4 py-3 text-left">
-                Question Count
-              </TableHead>
-              <TableHead className="px-4 py-3 text-left">Sections</TableHead>
-              <TableHead className="px-4 py-3 text-left">Created At</TableHead>
-              <TableHead className="px-4 py-3 text-left">Export</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {exams.map((exam) => (
-              <TableRow key={exam._id} className="border-t border-gray-100">
-                <TableCell className="px-4 py-2">{exam.title}</TableCell>
-                <TableCell className="px-4 py-2">{exam.category}</TableCell>
-                <TableCell className="px-4 py-2">{exam.duration}</TableCell>
-                <TableCell className="px-4 py-2">
-                  {exam.questionCount}
-                </TableCell>
-                <TableCell className="px-4 py-2">{exam.sectionCount}</TableCell>
-                <TableCell className="px-4 py-2">
-                  {formatDate(
-                    exam.createAt?.toString() ||
-                      getDateFromObjectId(exam._id).toLocaleString()
-                  )}
-                </TableCell>
-                <TableCell className="px-4 py-2">
-                  <Button
-                    onClick={() => handleExport(exam._id)}
-                    disabled={exporting === exam._id}
-                    className="px-4 py-2 text-white bg-gray-600 rounded hover:bg-gray-700 disabled:bg-gray-400"
-                  >
-                    {exporting === exam._id ? (
-                      <span>Exporting...</span>
-                    ) : (
-                      <PiExport className="w-5 h-5" /> // Using the icon here
+        <div className="overflow-x-auto">
+          <Table className="min-w-full bg-white border border-gray-200 rounded-lg">
+            <TableHeader>
+              <TableRow className="bg-gray-200">
+                <TableHead className="px-4 py-3 text-left text-sm font-medium text-gray-700 uppercase tracking-wider">
+                  Title
+                </TableHead>
+                <TableHead className="px-4 py-3 text-left text-sm font-medium text-gray-700 uppercase tracking-wider">
+                  Category
+                </TableHead>
+                <TableHead className="px-4 py-3 text-left text-sm font-medium text-gray-700 uppercase tracking-wider">
+                  Duration (mins)
+                </TableHead>
+                <TableHead className="px-4 py-3 text-left text-sm font-medium text-gray-700 uppercase tracking-wider">
+                  Question Count
+                </TableHead>
+                <TableHead className="px-4 py-3 text-left text-sm font-medium text-gray-700 uppercase tracking-wider">
+                  Sections
+                </TableHead>
+                <TableHead className="px-4 py-3 text-left text-sm font-medium text-gray-700 uppercase tracking-wider">
+                  Created At
+                </TableHead>
+                <TableHead className="px-4 py-3 text-left text-sm font-medium text-gray-700 uppercase tracking-wider">
+                  Export
+                </TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {exams.map((exam) => (
+                <TableRow key={exam._id} className="hover:bg-gray-100">
+                  <TableCell className="px-4 py-2 text-gray-800">
+                    {exam.title}
+                  </TableCell>
+                  <TableCell className="px-4 py-2 text-gray-800">
+                    {exam.category}
+                  </TableCell>
+                  <TableCell className="px-4 py-2 text-gray-800">
+                    {exam.duration}
+                  </TableCell>
+                  <TableCell className="px-4 py-2 text-gray-800">
+                    {exam.questionCount}
+                  </TableCell>
+                  <TableCell className="px-4 py-2 text-gray-800">
+                    {exam.sectionCount}
+                  </TableCell>
+                  <TableCell className="px-4 py-2 text-gray-800">
+                    {formatDate(
+                      exam.createAt?.toString() ||
+                        getDateFromObjectId(exam._id).toLocaleString()
                     )}
-                  </Button>
+                  </TableCell>
+                  <TableCell className="px-4 py-2">
+                    <Button
+                      onClick={() => handleExport(exam._id)}
+                      disabled={exporting === exam._id}
+                      className="flex items-center justify-center px-3 py-1 bg-gray-800 text-white rounded-md hover:bg-gray-950 transition-colors disabled:bg-blue-300"
+                    >
+                      {exporting === exam._id ? (
+                        <svg
+                          className="animate-spin h-5 w-5 text-white"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                        >
+                          <circle
+                            className="opacity-25"
+                            cx="12"
+                            cy="12"
+                            r="10"
+                            stroke="currentColor"
+                            strokeWidth="4"
+                          ></circle>
+                          <path
+                            className="opacity-75"
+                            fill="currentColor"
+                            d="M4 12a8 8 0 018-8v8z"
+                          ></path>
+                        </svg>
+                      ) : (
+                        <PiExport className="w-5 h-5" />
+                      )}
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+            <TableFooter>
+              <TableRow>
+                <TableCell colSpan={7} className="px-4 py-3 text-right">
+                  <PaginationCustom
+                    currentPage={currentPage}
+                    totalPages={totalPages}
+                  />
                 </TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-          <TableFooter>
-            <TableRow>
-              <TableCell colSpan={7} className="px-4 py-3 text-right">
-                Page {currentPage} of {totalPages}
-              </TableCell>
-            </TableRow>
-          </TableFooter>
-        </Table>
-      )}
-
-      {/* Pagination */}
-      {totalPages > 1 && (
-        <PaginationCustom currentPage={currentPage} totalPages={totalPages} />
+            </TableFooter>
+          </Table>
+        </div>
       )}
     </div>
   );

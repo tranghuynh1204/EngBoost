8add3a4590bc68b9753ce1b5c63206685c0ff88a
JAVA_09_21110856 <21110856@student.hcmute.edu.vn>
2025-02-26 21:44:48 +0700

    restyle of exam page and add search input

--- toeic-online/components/exam/exam-card.tsx
+++ toeic-online/components/exam/exam-card.tsx
@@ -4,29 +4,12 @@ import { AiOutlineComment } from "react-icons/ai";
 import { GiSpellBook } from "react-icons/gi";
 import { FiUsers } from "react-icons/fi";
 import { TbUserStar } from "react-icons/tb";
-import { useEffect, useState } from "react";
 interface ExamCardProps {
   exam: Exam; // Define exam as the prop
 }
 const ExamCard = ({ exam }: ExamCardProps) => {
-  const [hasAttempted, setHasAttempted] = useState(false);
-
-  useEffect(() => {
-    const fetchExamStatus = async () => {
-      try {
-        const response = await fetch(`/user-exams/exam/${exam._id}/status`);
-        const data = await response.json();
-        console.log(data);
-        setHasAttempted(data.attempted);
-      } catch (error) {
-        console.error("Error fetching exam status:", error);
-      }
-    };
-
-    fetchExamStatus();
-  }, [exam._id]);
   return (
-    <div className="bg-slate-50 rounded-xl shadow-md p-4 hover:shadow-xl transition-shadow duration-300 w-49 border border-gray-200">
+    <div className="bg-slate-50 rounded-xl p-4 border border-slate-400 transition-transform transform hover:scale-105 hover:shadow-lg hover:border-slate-600 duration-300 ease-in-out">
       <div className="text-center mb-3">
         <h2 className="text-md font-semibold text-gray-800">{exam.title}</h2>
       </div>
@@ -60,12 +43,10 @@ const ExamCard = ({ exam }: ExamCardProps) => {
           </span>
         </div>
 
-        <div className="flex items-center space-x-2 text-gray-600 text-xs">
-          <TbUserStar
-            className={`w-4 h-4 ${hasAttempted ? "text-green-600" : "text-blue-400"}`}
-          />
-          <span className={hasAttempted ? "text-green-600 font-semibold" : "text-gray-600"}>
-            {exam.userCount}
+        <div className="flex items-center space-x-2  text-xs">
+          <TbUserStar className="w-4 h-4 text-blue-400" />
+          <span>
+            <span className="font-semibold">User: </span> {exam.userCount}
           </span>
         </div>
       </div>

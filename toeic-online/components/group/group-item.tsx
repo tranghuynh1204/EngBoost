5eada795e8187efa281ff6fc5ed55b0b07508cbd
JAVA_09_21110856 <21110856@student.hcmute.edu.vn>
2025-03-01 13:12:45 +0700

    Refine Practice Page include Radio Item, Tracker, Group Item

--- toeic-online/components/group/group-item.tsx
+++ toeic-online/components/group/group-item.tsx
@@ -8,7 +8,7 @@ interface GroupItemProps {
 
 export const GroupItem = ({ group }: GroupItemProps) => {
   return (
-    <div className="space-y-6">
+    <div className="space-y-4">
       {/* Audio Player */}
       {group.audio && (
         <audio

87343c484e416d8282ea6b54ec5bd6126be6b57e
JAVA_09_21110856 <21110856@student.hcmute.edu.vn>
2025-03-01 21:11:46 +0700

    Refine UI Flashcard lists

--- toeic-online/components/flashcard/flashcard-item.tsx
+++ toeic-online/components/flashcard/flashcard-item.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import React from "react";
+import { TbBook, TbId } from "react-icons/tb";
 export interface FlashcardItemProps {
   _id: string;
   title: string;
@@ -14,12 +15,42 @@ export const FlashcardItem = ({
 }: FlashcardItemProps) => {
   return (
     <Link href={`/flashcards/${_id}`}>
-      <div className="p-4 bg-white border rounded-lg shadow-md hover:shadow-lg transition-shadow cursor-pointer">
-        <div className="text-lg font-semibold text-[rgb(53,47,68)]">
-          {title}
+      <div
+        className="
+        flex flex-col justify-between
+        bg-slate-50
+        w-[230px]
+        h-[180px]
+        rounded-lg
+        p-4
+        border
+        border-slate-300
+        hover:border-slate-400
+        transition-transform
+        transform
+        hover:scale-105
+        hover:shadow-sm
+        duration-200
+        ease-in-out
+        cursor-pointer
+      "
+      >
+        {/* Icon + Title */}
+        <div className="flex items-center space-x-3">
+          <div className="flex items-center justify-center w-10 h-10  bg-sky-100 rounded-lg">
+            <TbId className="text-sky-600" size={20} />
+          </div>
+          <h2 className="text-sm font-semibold text-gray-700">{title}</h2>
         </div>
-        <div className="text-sm text-gray-500">{vocabularyCount} words</div>
-        <div className="text-sm text-gray-700 mt-2">{description}</div>
+
+        {/* Description */}
+        <p className="mt-2 text-xs text-gray-500">{description}</p>
+
+        {/* Horizontal rule + word count */}
+        <hr className="my-2 border-gray-200" />
+        <div className="text-xs text-gray-500 hover:underline">
+          {vocabularyCount} words
+        </div>
       </div>
     </Link>
   );
